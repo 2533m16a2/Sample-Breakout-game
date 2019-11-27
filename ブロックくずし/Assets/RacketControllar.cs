8c8908b8536f83b8eacb422ad87750b033c920ef
2533m16a2 <46391465+2533m16a2@users.noreply.github.com>
2019-11-27 13:31:16 +0900

    sample breakout
    sample breakoutの追加

---
+++ ブロックくずし/Assets/RacketControllar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketControllar : MonoBehaviour
+{
+    public int Accel = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.right * Input.GetAxisRaw("Horizontal") * Accel, ForceMode.Impulse);
+    }
+}
---
+++ ブロックくずし/Assets/ballcollider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballcollider : MonoBehaviour
+{
+    public int speed = 20;
+    private Rigidbody _rigidbody;
+    Vector3 pos0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce((transform.forward + transform.right) * speed, ForceMode.VelocityChange);
+        _rigidbody = this.GetComponent<Rigidbody>();
+        Transform myTransform = this.transform;
+        pos0 = myTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Transform myTransform = this.transform;
+            myTransform.position = pos0;
+            GetComponent<Rigidbody>().AddForce((transform.forward + transform.right) * speed, ForceMode.VelocityChange);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        _rigidbody.velocity *= 28f/_rigidbody.velocity.magnitude;
+    }
+}
